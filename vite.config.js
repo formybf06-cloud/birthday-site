0b031dbbd2fd97fb6249715d1df297637dac6143
ashiyaaa121 <khatriashiya16@gmail.com>
2026-01-03 19:09:36 +0545

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/birthday-site/',
   plugins: [react()],
+  base: '/birthday-site/',
 })
+
 
